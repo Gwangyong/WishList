d5e62eddd7dbfe373c94c6b78d7f815af1c1696f
gwangyong <key4168@icloud.com>
2022-05-05 22:16:39 +0900

    first commit & .gitignore file

---
+++ WishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishList
+//
+//  Created by 서광용 on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
